7b683405382ca7c89bb137567601015454a9f2ef
Kacper Rowiński <kacper.rowinski@getresponse.com>
2015-04-08 17:46:24 +0200

    up

---
+++ src/XSLT/XSLTFactory.php
@@ -0,0 +1,82 @@
+<?php
+namespace Fazi\LaravelXSLT;
+
+use ReflectionClass;
+use Illuminate\View\Factory;
+use Illuminate\View\ViewFinderInterface;
+use Illuminate\View\Engines\EngineResolver;
+use Illuminate\Contracts\Config\Repository as ConfigContract;
+use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
+
+
+class XSLTFactory extends Factory
+{
+
+    /**
+     * @var string  version
+     */
+    const VERSION = '2.0.0-dev';
+
+    /** @var Smarty $smarty */
+    protected $smarty;
+
+    /** @var ConfigContract $config */
+    protected $config;
+
+    /**
+     * @param EngineResolver $engines
+     * @param ViewFinderInterface $finder
+     * @param DispatcherContract $events
+     * @param Smarty $smarty
+     * @param ConfigContract $config
+     */
+    public function __construct(EngineResolver $engines, ViewFinderInterface $finder, DispatcherContract $events, Smarty $smarty, ConfigContract $config)
+    {
+        parent::__construct($engines, $finder, $events);
+        $this->smarty = $smarty;
+        $this->config = $config;
+        $this->setConfigure();
+    }
+
+    /**
+     * @return \Smarty
+     */
+    public function getSmarty()
+    {
+        return $this->xslt;
+    }
+
+    /**
+     * @return string
+     */
+    public function getVersion()
+    {
+        return self::VERSION;
+    }
+
+    /**
+     * smarty configure
+     * @access private
+     * @return void
+     */
+    private function setConfigure()
+    {
+
+    }
+
+    /**
+     * @param $name
+     * @param $arguments
+     * @return mixed
+     * @throws MethodNotFoundException
+     */
+    public function __call($name, $arguments)
+    {
+        $reflectionClass = new ReflectionClass($this->smarty);
+        if (!$reflectionClass->hasMethod($name))
+        {
+            throw new MethodNotFoundException("{$name} : Method Not Found");
+        }
+        return call_user_func_array([$this->smarty, $name], $arguments);
+    }
+}
---
+++ src/XSLT/XSLTServiceProvider.php
@@ -0,0 +1,104 @@
+<?php
+namespace Fazi\LaravelXSLT;
+
+use Illuminate\Support\ServiceProvider;
+
+class XSLTServiceProvider extends ServiceProvider
+{
+
+    /**
+     * boot process
+     */
+    public function boot()
+    {
+        // register template cache driver
+        $this->registerCacheStorage();
+        // register commands
+        $this->registerCommands();
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $configPath = __DIR__ . '/../config/ytake-laravel-smarty.php';
+        $this->mergeConfigFrom($configPath, 'ytake-laravel-smarty');
+        $this->publishes([$configPath => config_path('ytake-laravel-smarty.php')]);
+
+        $this->app['view'] = $this->app->share(function ($app)
+        {
+            $factory = new SmartyFactory($app['view.engine.resolver'], $app['view.finder'], $app['events'], new Smarty, $this->app['config']);
+
+            // Pass the container to the factory so it can be used to resolve view composers.
+            $factory->setContainer($app);
+
+            return $factory;
+        });
+
+        // add smarty extension (.tpl)
+        $this->app['view']->addExtension($this->app['config']->get('laravel-smarty.extension', 'tpl'), 'smarty', function ()
+        {
+            return new Engines\SmartyEngine($this->app['view']->getSmarty());
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            'command.ytake.laravel-smarty.clear.compiled',
+            'command.ytake.laravel-smarty.clear.cache',
+            'command.ytake.laravel-smarty.optimize',
+            'command.ytake.laravel-smarty.info',
+        ];
+    }
+
+    /**
+     * @return void
+     */
+    protected function registerCommands()
+    {
+        // Package Info command
+        $this->app['command.ytake.laravel-smarty.info'] = $this->app->share(function ()
+        {
+            return new Console\PackageInfoCommand;
+        });
+        // cache clear
+        $this->app['command.ytake.laravel-smarty.clear.cache'] = $this->app->share(function ($app)
+        {
+            return new Console\CacheClearCommand($app['view']->getSmarty());
+        });
+        // clear compiled
+        $this->app['command.ytake.laravel-smarty.clear.compiled'] = $this->app->share(function ($app)
+        {
+            return new Console\CompiledClearCommand($app['view']->getSmarty());
+        });
+        // clear compiled
+        $this->app['command.ytake.laravel-smarty.optimize'] = $this->app->share(function ($app)
+        {
+            return new Console\CompiledCommand($app['view']->getSmarty(), $app['config']);
+        });
+        $this->commands([
+                'command.ytake.laravel-smarty.clear.compiled',
+                'command.ytake.laravel-smarty.clear.cache',
+                'command.ytake.laravel-smarty.optimize',
+                'command.ytake.laravel-smarty.info',
+            ]);
+    }
+
+    /**
+     * @return Storage
+     */
+    protected function registerCacheStorage()
+    {
+        return new Storage($this->app['view']->getSmarty(), $this->app['config']);
+    }
+
+}
